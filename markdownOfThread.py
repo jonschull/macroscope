eb91b8cf34edc1bca2c4b9f6f6ff4c55db75d6c9
jonschull <Jschull@gmail.com>
2020-02-15 14:13:51 -0500

    new code new data (dump.json from Mazvy)
    markdownOfThread replaces markdowfiy deals with dump.json
    macros module contains new figures (using plotly px) using dump.json

---
+++ markdownOfThread.py
@@ -0,0 +1,45 @@
+#derived from markdownize.py
+def avatarImg(username=''):
+    return f'http://res.cloudinary.com/e-nable-org/image/upload/w_150,r_max/{username}'                         #show it
+
+def getPostsFromThread(df, ID = 11):
+    roundvalue = round(ID)
+    return [post for post in df.to_dict('records') if roundvalue <= float(post['id']) < roundvalue + 1]
+
+def markdownOfThread(df, ID=11.3):
+    from markdownify import markdownify as md
+    RET='\n'
+    posts = getPostsFromThread(df, ID)
+    title = f"""**{posts[0]['body'][:80]}...**
+    
+Category:{posts[0]['topics']}
+
+{str(posts[0]['datetime'])[:-15]} | {str(posts[0]['datetime'])[11:16]}
+___
+"""
+            
+    content = [f'\n{title}\n\n']
+    for post in posts:
+        postBody = md(post['body'])#.replace('<p>',RET).replace('</p>',RET)
+        content.append(
+f"""
+![avatar]({avatarImg(post['name'])})  {post['name']}  
+
+{str(post['datetime'])[:-15]} | {str(post['datetime'])[11:16]}
+
+{postBody}
+___
+""")
+
+    markdown = '\n'.join(content)
+    return markdown
+
+if __name__=='__main__':
+    
+    import pandas as pd
+    df = pd.read_json('dump.json')
+    df['name'] = [user.replace('(Legacy) ', '') for user in df['user']] 
+
+
+    from IPython.display import Markdown
+    Markdown(df, markdownOfThread(df, 6))
