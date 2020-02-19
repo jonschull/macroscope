61d7110a3109b3010b24edb2a6b19256aef7493a
jonschull <Jschull@gmail.com>
2020-02-18 21:20:24 -0500

    Major refactoring; makefig takes df as parameter
    (which will allow us to pass filtered data)
    new layout utlity is very helpful

--- macrosModule.py
+++ macrosModule.py
@@ -33,11 +33,13 @@ df['name'] = [user.replace('(Legacy) ', '') for user in df['user']]
 df['text'] = [tup.name + ' ' + str(tup.datetime)[:19] for tup in df.itertuples()] #Jon Schull 2013-07-10 20:01:06
 
 
-# # Markdown
+# ## Markdown
 
 from IPython.display import Markdown
 from markdownOfThread import markdownOfThread
 #Markdown(markdownOfThread(df,9))
+
+# ## addTo the dataFrame
 
 df['threadY'] = (df.id - round(df.id) * 100000)
 df['name'] = [user.replace('(Legacy) ', '') for user in df['user']] 
@@ -71,136 +73,179 @@ df['UNN'] = UNNs
 df['UNNcolor'] = df['UNN'] % 10
 df['UNNcolor'] = df['UNNcolor'].astype(str)
 df['smThreadPos'] = df.threadPos / 1000
+
+
 # -
-# # ThreadsFig, colored by threadID
+# ## makeCustomXaxis for the bothFig
 
-# +
-fig = px.scatter(df, x='datetime', y='id', hover_data=['id'], color='IDcolor', size='threadPos', height=400, hover_name='name', opacity=0.4, template='plotly_dark')
-fig.update_layout(showlegend=False)
-fig.update_xaxes(
-     rangeslider=dict(visible=True, thickness=0.02, bgcolor='yellow')  
-)
-fig.update_yaxes(title_text='Successive Threads', fixedrange=False)
+def makeCustomXaxis(df): #for bothFig
+    df = df.sort_values('datetime')
+    #jonrows = df.query('UNN==0')
+    recs = df.to_dict('records')  
 
-threadsFig = fig
-#fig
-# -
-
-
-
-# # PeopleFig
-
-
-
-
-# +
-fig = px.scatter(df,x='datetime', y='UNN', hover_data=['id'], color='IDcolor',size='smThreadPos', height=400, hover_name='text', opacity=0.3, template='plotly_dark')
-fig.update_layout(showlegend=False)
-fig.update_xaxes(
-     rangeslider=dict(visible=True, thickness=0.02, bgcolor='yellow')  
-)
-fig.update_yaxes(title_text='People ordered by first post',  fixedrange=False)
-
-peopleFig = fig
-#peopleFig
-# -
-
-# # bothfig with custom xAxis
-
-# +
-#make customAxis 
-
-df = df.sort_values('datetime')
-#jonrows = df.query('UNN==0')
-recs = df.to_dict('records')  
-
-tickIDs = []
-ticktext = []
-for i, row in enumerate(recs):
-    if i==0: 
-        tickIDs.append(row['id'])
-        ticktext.append(row['datetime'])
-    else:
-        if row['datetime'].day != recs[i-1]['datetime'].day:
+    tickIDs = []
+    ticktext = []
+    for i, row in enumerate(recs):
+        if i==0: 
             tickIDs.append(row['id'])
             ticktext.append(row['datetime'])
+        else:
+            if row['datetime'].day != recs[i-1]['datetime'].day:
+                tickIDs.append(row['id'])
+                ticktext.append(row['datetime'])
 
-ticktext= [str(tt)[:10] for tt in ticktext]
+    ticktext= [str(tt)[:10] for tt in ticktext]
 
 
-downsample = 20
-ticktext = [tt for i,tt in enumerate(ticktext) if i%downsample == 0 ]
-tickIDs = [id for i, id in enumerate(tickIDs)  if i%downsample == 0 ]
+    downsample = 20
+    ticktext = [tt for i,tt in enumerate(ticktext) if i%downsample == 0 ]
+    tickIDs = [id for i, id in enumerate(tickIDs)  if i%downsample == 0 ]
 
-            
-customXaxis=dict(
-                             tickmode = 'array',
-                             tickvals = tickIDs,
-                             ticktext = ticktext
-                   )
 
-            
-fig = px.scatter(df, y='UNN', x='id',hover_data=['id'], color='IDcolor', height=400, size='smThreadPos', hover_name='text', opacity=0.3, template='plotly_dark')
-fig.update_layout(showlegend=False,
-                  xaxis = customXaxis
+    customXaxis=dict(
+                                 tickmode = 'array',
+                                 tickvals = tickIDs,
+                                 ticktext = ticktext
+                       )
+    return customXaxis
+
+
+# ##  makeFigs
+
+# +
+def makeFig(df, x='datetime', y='UNN', customAxis=False, 
+            plotTitle='', yTitle='',color='IDcolor'):
+    
+    fig = px.scatter(df,x=x, y=y, hover_data=['id'], 
+                     color= color,
+                     size='smThreadPos', 
+                     height=400, hover_name='text',
+                     opacity=0.3, template='plotly_dark')
+    
+    fig.update_layout(showlegend=False, title=plotTitle)
+    
+    fig.update_xaxes(
+         rangeslider=dict(visible=True, thickness=0.02, bgcolor='yellow')  )
+    
+    fig.update_yaxes(title_text=yTitle,  fixedrange=False)
+    
+    if customAxis:
+        fig.update_xaxes(title_text = 'Successive Threads over Time')
+        fig.update_layout(showlegend=False,
+                  xaxis = makeCustomXaxis(df)
                  )
-fig.update_xaxes(title_text='Successive Threads over time.', 
-                 rangeslider=dict(visible=True, thickness=0.02, bgcolor='yellow'),
-                )
-fig.update_yaxes(title_text='People ordered by first post', fixedrange=False)
+    return fig
 
-bothFig = fig
 
-#bothFig
+peopleFig = makeFig(df, x='datetime', y='UNN', color='IDcolor',
+                        plotTitle='People over time. Colored by Thread',
+                        yTitle = 'People in order of appearance.')
+
+threadsFig = makeFig(df, x='datetime', y='id', color='UNNcolor',
+                       plotTitle='Threads over time. Colored by Person',
+                       yTitle = 'Posts in order of appearance')
+
+bothFig =  makeFig (df, x='id', y='UNN', customAxis=True,
+                        plotTitle='People over Time. Vertical bands of color are threads',
+                        yTitle = 'People in order of appearance')
+
+#bothFig                    
 # -
 # # Dash App! 
-# requires the dash jupyter_lab extension in Jupyter be pip installed and enabled in Extension Mgr
+# in Jupyter, requires the dash jupyter_lab extension (pip installed and enabled in Extension Mgr)
 
-# # inJupyter?
-# ## when used with the jupytext extension, this is intended to allow me to run 
-# python macrosmodule.py (ipython doen't work because of the extensions own clever check own  )
-# and 
+# ## Develop and test Components
 
-import jupyterlab_dash #https://github.com/plotly/jupyterlab-dash
-#TEMP
-import dash
-app = dash.Dash(__name__)
+# #### in Jupyter?
 
+# +
+import sys    
+inJupyter = sys.argv[-1].endswith('json')
+print(f'inJupyter: {inJupyter}')
 
+if inJupyter:
+    import jupyterlab_dash #https://github.com/plotly/jupyterlab-dash
 
-
-
+# -
 
 
 import dash
 import dash_html_components as html
-
-
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
-import visdcc
+import visdcc #used for javascript.  Not currently used
 
 
-from dash.dependencies import Input, Output, State
+# #### layout Tester 
 
 # +
-app = dash.Dash(__name__)
+def testLayoutInJupyter(layout=html.H1('hello')):
+    if not inJupyter:
+        exit()
+        
+    global viewer
+    
+    app=dash.Dash(__name__)
+    app.layout = layout
+    
+    if not 'viewer' in globals(): #create the viewer once
+        viewer = jupyterlab_dash.AppViewer() 
+        
+    viewer.show(app)
+    return viewer
 
+viewer = testLayoutInJupyter()
 
+#to destroy:
+#viewer.terminate()
+#del(viewer)
+# -
+
+# #### leftPanel
+
+leftPanel = dbc.Col([  html.H2("Selected Thread"),
+                           dcc.Markdown('selected thread __goes here__', id='HoverBox')],
+                    width=3,
+                )
+viewer = testLayoutInJupyter(leftPanel)
+
+# +
+#### dropDown
+# -
+
+dropDown = dcc.Dropdown(
+                        id='dropdown',
+                        options=[
+                            {'label': 'All three graphs', 'value': 'AT'},                            
+                            {'label': 'People and and their Posts, Color=Person', 'value': 'PP'},
+                            {'label': 'Threads with Comments, inner=Person, outer=Thread', 'value': 'PPT'},
+                            {'label': 'New Threads Over Time, Color=Person', 'value': 'TT'}
+                        ],
+                        value='AT'
+                    )
+viewer = testLayoutInJupyter(dropDown)
+
+# #### twinRow 
+# (side by side People and Thread figs)
+
+# +
+twinRow= dbc.Row(children = [ dbc.Col(width=6, children = [ dcc.Graph(id='peopleFig', figure=peopleFig), ]),
+                              dbc.Col(width=6, children = [ dcc.Graph(id='threadFig', figure=threadsFig) ])], 
+                 id= 'twinRow')
+
+viewer = testLayoutInJupyter(twinRow)
+# -
+
+# #### right Panel including Cfilter and TwinRow
+
+# +
 Cfilter = dcc.Input(id='Cfilter',
                        placeholder='word or phase...',
                        type='text',
-                       value=''
-                   )
+                       value='' )
 
-
-body = dbc.Container([
-        dbc.Row([
-                dbc.Col([  html.H2("Selected Thread"),
-                           dcc.Markdown('selected thread __goes here__', id='HoverBox')],
-                    md=4,
-                ),
-                dbc.Col([   
+rightPanel = dbc.Col(width=8,
+                    children = [   
                         html.H1('These are data from G+ and Wikifactory...'),
                         html.Br(),
                         html.Span('Filter posts by Content or Author:  '),            
@@ -209,40 +254,45 @@ body = dbc.Container([
 * **Mouse over** a data point to see author and date.
 * **Click** on a data point to view the Thread or **>>>Participate** at hub.e-nable.org
                         """),
-                    
-                        visdcc.Run_js(id = 'javascript'),
-                        dbc.Row([     dbc.Col([
-                                            html.H1('People'),
-                                            dcc.Graph(id='peopleFig', figure=peopleFig),
-                                            html.P('Horizontal Bands are People. Dots are posts.'),
-
-                                            ]
-                                    ),
-                                            
-                                      dbc.Col([
-                                            html.H1('Threads'),
-                                            dcc.Graph(id='threadFig', figure=threadsFig),
-                                            html.P('Horizontal Bands are Threads. Dots are posts.'),
-
-                                      ]
-                                    )
-                                ]),
+ 
+                   
+                       dropDown,                        
+                       visdcc.Run_js(id = 'javascript'),
+                        twinRow,
                         
                         html.H1('People and Threads'),
                         html.P('Horizontal Banda are People.  Vertical columns of the same color are Threads.'),
                         dcc.Graph(id='bothFig', figure=bothFig)
-                    ]),
-             ])
-    ],className="mt-4",)
+                    ])
+
+testLayoutInJupyter(rightPanel)
+# -
+# #### both Panels
+
+# +
+bothPanels = dbc.Row([
+                leftPanel,
+                rightPanel ])
+
+testLayoutInJupyter(bothPanels)
+# -
 
 
 
+
+# ## App with Callbacks
+
+# +
+app = dash.Dash(__name__)
+
+body = bothPanels
 
 app = dash.Dash(__name__, external_stylesheets=['assets/bootstrap-grid.min'])  ### now uses css in assets dir?
+
 app.layout = html.Div(body)
 
 
-
+from dash.dependencies import Input, Output, State
 @app.callback(
     Output('HoverBox' , 'children'),
     [Input('threadFig', 'clickData'),
@@ -250,9 +300,7 @@ app.layout = html.Div(body)
      Input('bothFig'  , 'clickData')
     ]
 )
-
-
-def updateSpan(tdata, pdata, bdata):
+def updateThreadBox(tdata, pdata, bdata):
     ctx = dash.callback_context
     if ctx.triggered:
         if ctx.triggered[0]['value']:
@@ -267,35 +315,14 @@ def updateSpan(tdata, pdata, bdata):
     return '(click on a data point)'
     
 
-    
-import sys    
-inJupyter = sys.argv[-1].endswith('json')
-print(f'inJupyter: {inJupyter}')
-
-if inJupyter:
-    if not 'viewer' in globals().keys():
-        viewer = jupyterlab_dash.AppViewer() #create the viewer once
+if inJupyter:  #this allows running via python 
+    if not 'viewer' in globals().keys():   #create the viewer once
+        viewer = jupyterlab_dash.AppViewer()
     viewer.show(app)
 else:
     if __name__ == "__main__":
         app.run_server(debug=True)
 
 # -
-globals().keys()
-
-
-viewer
-
-viewer.show(app)
-
-viewer
-
-viewer.terminate()
-
-
-viewer
-
-
-
 
 
